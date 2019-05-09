af891edca012779922a129e5b50bbbea2d43515e
Pontus <Pontus.fredriksson1@gmail.com>
2019-05-09 14:30:19 +0200

    Prototype gameEngine created as mutation "turnFinished"

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,39 +12,88 @@ export default new Vuex.Store({
     //3:Game Over
     gameState:0,
     totalMatchTime:50,
-    movehistory: {
-      currentQuestion:"change to a template question",
+    currentPlayerIndex:0,
+    currentQuestion:{
+      userSubmitted:false,
+      author: "guest",
+      question:"How many developers work in the group six lobsters?",
+      answer:7,
+      low:1,
+      high:10
+    },
+    moveHistory: {
+      question:null,
       moves:[]
     },
     sessionPlayersArray:[
       {
-        name: TheUser,
+        databaseId:null,
+        name: "guest",
         isPlayer:true,
         wins:5,
         losses:7,
         description:"testPlayer and template",
-        imange:null,
+        image:null,
         timeleft:totalMatchTime,
         },
         {
-          name: botTemplate,
+          name: "botTemplate",
           isPlayer:false,
           wins:100,
           losses:300,
           description:"testBot and template",
-          imange:null,
+          image:null,
           timeleft:totalMatchTime,
-          move: function (movehistory){
-              Newmove: [
-              player=name
-            ]
-            return move
+          move() {
+              let lastMove= state.moveHistory[moveHistory.length-1];
+              let newMove= {
+                guess: lastMove.high-1,
+                timeTook:2
+              }
+            return newMove;
           }
         }
       ]
   },
   mutations: {
-
+    assignQuestion(state,question){
+      state.currentQuestion= question,
+      state.moveHistory.length=0,
+      state.moveHistory.push({
+        low:state.currentQuestion.low,
+        high:state.currentQuestion.high
+      })
+    },
+    turnFinished(state){
+      //if someone won:
+      if(state.moveHistory[moveHistory.length].guess==
+          state.currentQuestion.answer){
+            state.gameState=3;
+      }else{
+        //if last player
+        if(state.currentPlayerIndex==state.sessionPlayersArray.length-1){
+          state.currentPlayerIndex=0;
+        }else{
+          state.currentPlayerIndex++;
+        }
+        //if bots turn
+        if(!state.sessionPlayersArray[state.currentPlayerIndex].isPlayer){
+          addMove(state.sessionPlayersArray[state.currentPlayerIndex].move)
+          //recursive
+          state.turnFinished();
+        }
+      }
+    },
+    addMove(state, newMove){
+        //pushes last object in array to the same array
+        state.moveHistory.push(
+          state.moveHistory[state.moveHistory[state.movieHistory.length-1]]);
+        //forEach Propertyname in newMove
+        Object.keys(newMove).forEach(function (element){
+          //set movehistory properties to newMove properties
+          state.moveHistory[length-1][element]=newMove[element];
+        })
+    }
   },
   actions: {}
 });
