88840a759fd76f15db9f7cf5f71fd0a12a830ddc
Pontus <Pontus.fredriksson1@gmail.com>
2019-05-09 11:21:30 +0200

    Add mock Player and bot that doubles as templates.
    will still sit in vueX Store for next task, so
    dont go in there today, 2019/05/09 :)

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,47 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    timeoutMultiplier:1,
+    //0:No game
+    //1:Game starting
+    //2:Game in Progress
+    //3:Game Over
+    gameState:0,
+    totalMatchTime:50,
+    movehistory: {
+      currentQuestion:"change to a template question",
+      moves:[]
+    },
+    sessionPlayersArray:[
+      {
+        name: TheUser,
+        isPlayer:true,
+        wins:5,
+        losses:7,
+        description:"testPlayer and template",
+        imange:null,
+        timeleft:totalMatchTime,
+        },
+        {
+          name: botTemplate,
+          isPlayer:false,
+          wins:100,
+          losses:300,
+          description:"testBot and template",
+          imange:null,
+          timeleft:totalMatchTime,
+          move: function (movehistory){
+              Newmove: [
+              player=name
+            ]
+            return move
+          }
+        }
+      ]
+  },
+  mutations: {
+
+  },
   actions: {}
 });
