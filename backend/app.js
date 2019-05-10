1a37113a00808fc55e19cddf8a328448fad469c0
vargen2 <hjaarn2@gmail.com>
2019-05-10 12:55:02 +0200

    get and post to localhost mongodb

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var questions = require('./routes/api/questions');
+
 
 var app = express();
 
@@ -14,7 +14,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/questions', questions);
 
 module.exports = app;
