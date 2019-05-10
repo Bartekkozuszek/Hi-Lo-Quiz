b3e2ecb390f639caa31aec914021690f6c9e7e03
vargen2 <hjaarn2@gmail.com>
2019-05-10 14:28:05 +0200

    config temp

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var questions = require('./routes/api/questions');
+var questions = require('./routes/api/questions')
 
+var app = express()
 
-var app = express();
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/v1/questions', questions)
 
-app.use('/api/v1/questions', questions);
-
-module.exports = app;
+module.exports = app
