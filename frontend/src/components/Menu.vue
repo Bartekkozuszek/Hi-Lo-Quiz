cf5f4f9dfa7c4f89d57e9514195907ec9204176f
Wuxxerno <45070726+wuxxerno@users.noreply.github.com>
2019-05-12 22:01:08 +0100

    separate logotype from menu bar and implement basic design from meeting

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -1,75 +1,87 @@
 <template>
-  <header class="header">
-    <div id="title">
-      <select id="subject">
-        <option value="22">{{ settings }}</option>
-      </select>
-      <h1>{{ banner }}</h1>
-      <button class="select-btn" id="sbt1">{{ newGame }}</button>
-    </div>
-  </header>
+    <header class="header">
+    <div id="title"><h1>Hi-Lo-Pros</h1></div> 
+<div class="flex-container">
+  <div><button class="select-btn" id="sbt1">{{user}}</button></div>
+  <div> <select id="subject">
+                    <option>{{settings}}</option>
+					</select></div>
+
+  <div><button class="select-btn" id="sbt1">Login</button></div>  
+</div>
+    </header>
 </template>
 
 <script>
 export default {
   name: "Menu",
   props: {
+    user: String,
     settings: String,
-    banner: String,
-    newGame: String
+	newGame: String
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-header {
-  background: pink;
+header{
 
-  padding: 1vh;
-  text-align: center;
-}
 
+	padding: 1vh;
+	text-align: center;
+
+	}
+	
 h1 {
-  color: WHITE;
-  font-size: 3vh;
-  display: inline;
-  padding: 1vh;
+	color: green;
+	font-size: 4vh;
+	display: inline;
+	padding: 1vh;
 
-  margin-top: 1vh;
-  margin-left: 5vh;
-}
+
+	}
 
 #title {
-  margin-left: auto;
-  margin-right: auto;
-  display: inline;
-  color: WHITE;
-  font-family: "Days One", sans-serif;
-  font-size: 3vh;
-}
+
+
+	background-color: yellow;
+	font-family: 'Days One', sans-serif;
+	font-size: 3vh;
+	}
 
 select {
-  color: WHITE;
-  font-size: 3vh;
-  display: inline;
-  padding: 1vh;
-  background-color: purple;
-  margin-top: 1vh;
-  margin-left: 5vh;
-  text-decoration: none;
-  border: none;
-}
+	color: WHITE;
+	font-size: 3vh;
+	display: inline;
+	padding: 1vh;
+	background-color:purple;
+
+
+	text-decoration: none;
+	border: none;
+	}
 
 .select-btn {
-  color: WHITE;
-  font-size: 3vh;
-  display: inline;
-  padding: 1vh;
-  background-color: purple;
-  margin-top: 1vh;
-  margin-left: 5vh;
-  text-decoration: none;
-  border: none;
+	color: WHITE;
+	font-size: 3vh;
+	display: inline;
+	padding: 1vh;
+	background-color:purple;
+
+	text-decoration: none;
+	border: none;
+	}
+.flex-container {
+  display: flex;
+  justify-content: center;
+  background-color: pink;
+  justify-content: space-between;
+}
+
+.flex-container > div {
+
+  text-align: center;
+
 }
 </style>
