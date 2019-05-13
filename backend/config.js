98256f41b4bbe0d457c96b9fc905fc711489eb70
vargen2 <hjaarn2@gmail.com>
2019-05-13 16:41:11 +0200

    format

--- backend/config.js
+++ backend/config.js
@@ -1,29 +1,30 @@
 //temp not used atm
-const env = process.env.NODE_ENV; // 'dev' or 'test'
+const env = process.env.NODE_ENV // 'dev' or 'test'
 
 const dev = {
- app: {
-   port: 3000
- },
- db: {
-   host: 'mongodb://localhost:27017/test_lobsters_questions',
-   name: 'db'
- }
-};
+  app: {
+    port: 3000
+  },
+  db: {
+    host: 'mongodb://localhost:27017/test_lobsters_questions',
+    name: 'db'
+  }
+}
 
 const test = {
- app: {
-   port: 3000
- },
- db: {
-   host: 'mongodb+srv://readwrite:[password]@cluster0-5u44s.mongodb.net/test?retryWrites=true',
-   name: 'test'
- }
-};
+  app: {
+    port: 3000
+  },
+  db: {
+    host:
+      'mongodb+srv://readwrite:[password]@cluster0-5u44s.mongodb.net/test?retryWrites=true',
+    name: 'test'
+  }
+}
 
 const config = {
- dev,
- test
-};
+  dev,
+  test
+}
 
-module.exports = config[env];
+module.exports = config[env]
