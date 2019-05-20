e1acab4850d65db5ed4722370f577e65466c7846
Pontus <Pontus.fredriksson1@gmail.com>
2019-05-20 13:57:58 +0200

    start working on guessresponse

---
+++ frontend/src/components/GuessResponse.vue
@@ -0,0 +1,24 @@
+<template>
+    <img src="responseLogic"></img>
+</template>
+
+<script>
+    export default {
+        name: "GuessResponse",
+        methods:{
+            responselogic:function(){
+
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+    img {
+        position: absolute;
+        left: 0px;
+        top: 0px;
+        z-index: -1;
+    }
+</style>
