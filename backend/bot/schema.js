a4df532b95aea6837e64d92cce2deff0b600a412
vargen2 <hjaarn2@gmail.com>
2019-05-22 12:13:44 +0200

    game statts misc

--- backend/bot/schema.js
+++ backend/bot/schema.js
@@ -1,5 +1,5 @@
 module.exports = {
-  botID: { type: String, required: true },
+  botID: { type: Number, required: true },
   wins: { type: Number, required: false, default: 0 },
   losses: { type: Number, required: true, default: 0 }
 }
