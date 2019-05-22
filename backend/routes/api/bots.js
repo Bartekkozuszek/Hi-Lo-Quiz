afe94bd77d041f126267793aaeae9e18be12225b
vargen2 <hjaarn2@gmail.com>
2019-05-22 19:41:28 +0200

    add amount parameter for GET /api/v1/bots

--- backend/routes/api/bots.js
+++ backend/routes/api/bots.js
@@ -9,21 +9,18 @@ const uri = config.URL
 mongoose.connect(uri, { useNewUrlParser: true })
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   let sort = helpers.parseSort(req.query.sort)
+  const amount = parseInt(req.query.amount)
 
-  Bot.find(function(err, result) {
-    if (err) {
-      res.json({ msg: err.message })
-    }
-  }).sort([[sort, -1]])
-    .exec(function(err, docs) {
-      if (err) {
-        res.json({ msg: err.message })
-      } else {
-        res.json(docs)
-      }
-    })
+  try {
+    const bots = await Bot.find()
+      .sort([[sort, -1]])
+      .limit(amount)
+    res.json(bots)
+  } catch (error) {
+    res.json({ msg: error.message })
+  }
 })
 
 router.get('/:id', function(req, res, next) {
