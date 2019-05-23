d1a6df0a52f254305a19d2c11f5b3fd96a014b3d
Wuxxerno <45070726+wuxxerno@users.noreply.github.com>
2019-05-23 18:27:36 +0100

    config package.json and router for babel setup

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,6 +4,7 @@ import Home from "./views/Home";
 import Rules from "./components/Rules";
 import SelectBots from "./views/SelectBots.vue";
 import Login from "./views/Login.vue";
+import About from "./views/About.vue";
 
 Vue.use(Router);
 
@@ -38,8 +39,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About
     }
   ]
 });
