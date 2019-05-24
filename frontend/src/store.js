c29a22bc0b956e0c1c6b0bb763cbf2706461b2ff
Adam Bremholm <adambremholm@gmail.com>
2019-05-24 13:22:30 +0200

    Add SelectCategory funktion

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -33,6 +33,8 @@ export default new Vuex.Store({
     wantLastMove: false,
     totalMatchTime: 30,
     matchesPlayed: 0,
+    selectedCategory: '',
+    categories: [],
     timesUp: false,
     showHighScore: false,
     currentPlayerIndex: 0,
@@ -302,6 +304,12 @@ export default new Vuex.Store({
       state.loadedQuestions = loadedQuestions;
 
       },
+    setCategories(state, loadedCategories) {
+      state.categories = loadedCategories;
+    },
+    setSelectedCategory(state, index) {
+      state.selectedCategory = state.categories[index]
+    },
       login(state, payload) {
           state.isLoggedIn = true,
           state.currentUser.id = payload._id,
@@ -359,7 +367,7 @@ export default new Vuex.Store({
       state.wantAnswers = false;
       axios
         .get(
-          serverURL+"/api/v1/questions?amount=20",
+          serverURL+"/api/v1/questions?amount=20&category=" + state.selectedCategory,
           {
             headers: {
               access_token: localStorage.access_token
@@ -418,6 +426,24 @@ export default new Vuex.Store({
           high: state.currentQuestion.high
         }
       ];
+    },
+    async loadCategories({state, commit}) {
+      axios
+          .get(
+              serverURL+"/api/v1/questions/categories",
+              {
+                headers: {
+                  access_token: localStorage.access_token
+                }
+              }
+          )
+          .then(r => r.data)
+          .then(categories => {
+            commit("setCategories", categories);
+          })
+          .catch(error => {
+            console.log(error);
+          });
     },
     turnFinished({ state, getters, dispatch }) {
       //if someone won:
